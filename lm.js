8d1113b411956a1d834e499aedc0ab5b74c4dd61
Derrick Conway <derrick.conway15@gmail.com>
2019-03-28 13:02:57 +0000

    swipe

--- lm.js
+++ lm.js
@@ -16,6 +16,7 @@ Cylon.robot({
       led2: { driver: "led", pin: 2, connection: "arduino" },//circle  motion
       led4: { driver: "led", pin: 4, connection: "arduino" },// key tap motion
       led6: { driver: "led", pin: 6, connection: "arduino" },// screen tap motion
+      led8: { driver: "led", pin: 8, connection: "arduino" },// swipe motion
       leapmotion: { driver: 'leapmotion' }
     },
 
@@ -41,14 +42,27 @@ Cylon.robot({
               break; 
                     // key tap motion
                 case "keyTap":
-                    console.log('Toggling led4 for keyTap Gesture');
+                    console.log('Turning led4 on for keyTap Gesture');
                     my.led4.toggle();
                 break;
                     // screen tap motion
                 case "screenTap":
-                console.log('Toggling led6 for screenTap Gesture');
+                console.log('Turning led6 on for screenTap Gesture');
                 my.led6.toggle();
                 break;
+                //swipe motion
+            case "Swipe":
+              switch (gesture.state){
+                case "start":
+                  console.log("turining led8 on for Swipe start");
+                  my.led8.turnOn();
+                  break;
+                case "stop":
+                  console.log("Turning led8 Off for Swipe Stop");
+                  my.led8.turnOff();
+                  break;
+              }
+          break;
         }
       });
     });
